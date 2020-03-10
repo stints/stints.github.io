b4dd5793832c9be243099f6bad3a9aeeec403ebc
John Stinson <jstinson.dev@gmail.com>
2020-03-10 14:42:04 -0500

    updated stylings

---
+++ precache-manifest.66a036b82e084b6947e67cc2138ed905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80caa018f6b8f7a974deae15fdd860da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41915cce174d4dcc2f21",
+    "url": "/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "e24df182eac105d6d518",
+    "url": "/static/css/main.6dab1a96.chunk.css"
+  },
+  {
+    "revision": "41915cce174d4dcc2f21",
+    "url": "/static/js/2.9d2d6a46.chunk.js"
+  },
+  {
+    "revision": "9617c35fefb3909e96d4735aba67fb26",
+    "url": "/static/js/2.9d2d6a46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e24df182eac105d6d518",
+    "url": "/static/js/main.70a199ac.chunk.js"
+  },
+  {
+    "revision": "a7e9ccf88856e83270fc",
+    "url": "/static/js/runtime-main.4c7859d6.js"
+  }
+]);
