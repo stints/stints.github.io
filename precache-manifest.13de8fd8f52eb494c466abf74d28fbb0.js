023b017a5b14bab7a53840a677190dd26a188a34
John Stinson <john.towery.stinson@gmail.com>
2020-03-10 12:41:02 -0500

    Updates

---
+++ precache-manifest.13de8fd8f52eb494c466abf74d28fbb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23aafb1d175938decc6d6b88d0c87c9",
+    "url": "/stints.github.io/index.html"
+  },
+  {
+    "revision": "7e167db85f964e2fe7a4",
+    "url": "/stints.github.io/static/css/2.aeed1b96.chunk.css"
+  },
+  {
+    "revision": "bd57674bc2d9a72a3ef3",
+    "url": "/stints.github.io/static/css/main.00da87bb.chunk.css"
+  },
+  {
+    "revision": "7e167db85f964e2fe7a4",
+    "url": "/stints.github.io/static/js/2.c02cf600.chunk.js"
+  },
+  {
+    "revision": "9617c35fefb3909e96d4735aba67fb26",
+    "url": "/stints.github.io/static/js/2.c02cf600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd57674bc2d9a72a3ef3",
+    "url": "/stints.github.io/static/js/main.7a72018d.chunk.js"
+  },
+  {
+    "revision": "b12b1fd34e301bc100b6",
+    "url": "/stints.github.io/static/js/runtime-main.07d358b3.js"
+  }
+]);
